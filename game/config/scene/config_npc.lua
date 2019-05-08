1b29d44eda4d0d9182fcf1b048b3883b42a365c4
大鹏 <278334996@qq.com>
2019-05-08 19:53:23 +0800

    monster patrol state

---
+++ game/config/scene/config_npc.lua
@@ -0,0 +1,9 @@
+local config = {
+	[1] = {
+		id = 1, name = "新手向导小明", res_id=3001, default_chat = [[小明的默认对话]],
+	},
+	[2] = {
+		id = 2, name = "小明的女朋友", res_id=3002, default_chat = [[小明女友的默认对话]],
+	},
+}
+return config
